a88e26bb8107400a8501b50fb2f0b94f73a0208a
Broderick Riopelle <broderio@umich.edu>
2024-07-09 16:55:59 -0400

    update

--- serverTest.py
+++ serverTest.py
@@ -1,23 +1,27 @@
-import asyncio
 from websockets.sync.server import serve
 from websockets.exceptions import ConnectionClosedError
 from threading import Thread, Lock
 import json
 from mbot_bridge.api import MBot
+import time
 
 mbot = MBot()
 in_msgs = []
 out_msgs = []
 in_msgs_lock = Lock()
 out_msgs_lock = Lock()
+isConnected = False
 
 def bridge(websocket):
-    global in_msgs, out_msgs
+    global in_msgs, out_msgs, isConnected
+
+    isConnected = True
+
 
     while True:
 
         try:
-            in_msg = websocket.recv(timeout=0.1)
+            in_msg = websocket.recv(timeout=0.01)
             with in_msgs_lock:
                 in_msgs.append(in_msg)
         except TimeoutError as e:
@@ -28,72 +32,66 @@ def bridge(websocket):
         with out_msgs_lock:
             if (len(out_msgs) > 0):
                 websocket.send(out_msgs.pop(0))
+
+    isConnected = False
+
+
+def parse_json_msg(message):
+    global mbot
+
+    json_data = json.loads(message)
+    if (json_data.get('cmd') == "drive"):
+        vx = json_data.get('args').get('vx')
+        vy = json_data.get('args').get('vy')
+        wz = json_data.get('args').get('wz')
+        mbot.drive(vx, vy, wz)
+        print(json_data.get('args'))
+    elif (json_data.get('cmd') == "stop"):
+        mbot.stop()
+        print("Stopping")
+    elif (json_data.get('cmd') == "reset_odometry"):
+        mbot.reset_odometry()
+        print("Resetting Odometry")
+    elif (json_data.get('cmd') == "drive_path"):
+        path = json_data.get('args').get('path')
+        mbot.drive_path(path)
         
 def parse_msgs():
-    global mbot, msgs
+    global mbot, in_msgs, out_msgs, isConnected
+
+    start_time = time.time()
     while True:
+        if not isConnected:
+            time.sleep(0.25)
+            continue
+
         with in_msgs_lock:
             if (len(in_msgs) > 0):
                 message = in_msgs.pop(0)
-            else:
-                continue
-
-        json_data = json.loads(message)
-        if (json_data.get('cmd') == "drive"):
-            vx = json_data.get('args').get('vx')
-            vy = json_data.get('args').get('vy')
-            wz = json_data.get('args').get('wz')
-            mbot.drive(vx, vy, wz)
-            print(json_data.get('args'))
-        elif (json_data.get('cmd') == "stop"):
-            mbot.stop()
-            print("Stopping")
-        elif (json_data.get('cmd') == "read_lidar"):
-            print("Reading Lidar")
+                parse_json_msg(message)
+        
+        if (time.time() - start_time) > 0.1:
+            start_time = time.time()
             ranges, thetas = mbot.read_lidar()
-            msg = json.dumps({
-                                    'ranges': ranges,
-                                    'thetas': thetas
-                                    }).encode()
-            with out_msgs_lock:
-                out_msgs.append(msg)
-        elif (json_data.get('cmd') == "read_odometry"):
             x, y, theta = mbot.read_odometry()
-            print("Reading Odometry")
-            client.send(json.dumps({
+            msg = json.dumps({
+                                'pose': {
                                     'x': x,
                                     'y': y,
                                     'theta': theta
-                                    }).encode())
-        elif (json_data.get('cmd') == "reset_odometry"):
-            mbot.reset_odometry()
-            print("Resetting Odometry")
-        elif (json_data.get('cmd') == "drive_path"):
-            path = json_data.get('args').get('path')
-            mbot.drive_path(path)
-            print("Driving Path")
-        elif (json_data.get('cmd') == "read_slam_pose"):
-            x, y, theta = mbot.read_slam_pose()
-            print("Reading SLAM Pose")
-            msg = json.dumps({
-                                'x': x,
-                                'y': y,
-                                'theta': theta
-                                }).encode()
-            with out_msgs_lock:
-                out_msgs.append(msg)
-        elif (json_data.get ('cmd') == "read_hostname"):
-            hostname = mbot.read_hostname()
-            msg = json.dumps({
-                                'hostname': hostname
-                                }).encode()
+                                },
+                                'scan': {
+                                    'ranges': ranges,
+                                    'thetas': thetas
+                                }
+                                })
             with out_msgs_lock:
                 out_msgs.append(msg)
                     
 def main():
     parse_thread = Thread(target=parse_msgs, name='parse_thread', daemon=True)
     parse_thread.start()
-    with serve(bridge, "localhost", 8765) as server:
+    with serve(bridge, "0.0.0.0", 8765) as server:
         server.serve_forever()
 
 if __name__ == '__main__':
