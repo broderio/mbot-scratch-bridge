722f50a7b6ed936cad67fa152b1bdc66c5bc6d77
Broderick Riopelle <broderio@umich.edu>
2024-07-09 13:19:13 -0400

    update to websocket

--- clientTest.py
+++ clientTest.py
@@ -1,31 +1,32 @@
-import socket
+import asyncio
+from websockets.sync.client import connect
 from time import sleep
 import json
 
-# Create a socket client
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+with connect("ws://localhost:8765") as websocket:
+    # Send data to the server
+    for i in range(4):
+        websocket.send(json.dumps({'cmd': "drive",
+                                "args": {
+                                    "vx": 0.0,
+                                    "vy": 0.0,
+                                    "wz": 0.0
+                                    }
+                                }).encode())
+        sleep(1)
 
-# Connect to the server
-client.connect(('localhost', 1234))
-
-# Send data to the server
-for i in range(5):
-    client.send(json.dumps({'cmd': "drive",
-                            "args": {
-                                "vx": 1.0,
-                                "vy": 0.0,
-                                "wz": 0.0
-                                }
-                            }).encode())
+    websocket.send(json.dumps({'cmd': "drive",
+                                "args": {
+                                    "vx": 0.0,
+                                    "vy": 0.0,
+                                    "wz": 0.0
+                                    }
+                                }).encode())
     sleep(1)
 
-for i in range(5):
-    client.send(json.dumps({'cmd': "read_lidar"}).encode())
-    sleep(1)
-    data = client.recv(1024)
-    json_data = json.loads(data.decode())
-    print(json_data)
-
-
-# Close the connection
-client.close()
+    for i in range(5):
+        websocket.send(json.dumps({'cmd': "read_lidar"}).encode())
+        sleep(1)
+        data = websocket.recv()
+        json_data = json.loads(data.decode())
+        print(json_data)
--- serverTest.py
+++ serverTest.py
@@ -1,25 +1,44 @@
-import socket
+import asyncio
+from websockets.sync.server import serve
+from websockets.exceptions import ConnectionClosedError
+from threading import Thread, Lock
 import json
+from mbot_bridge.api import MBot
 
-def main():
-    # Create a socket server
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+mbot = MBot()
+in_msgs = []
+out_msgs = []
+in_msgs_lock = Lock()
+out_msgs_lock = Lock()
 
-    # Bind the server to the address and port
-    server.bind(('localhost', 1234))
+def bridge(websocket):
+    global in_msgs, out_msgs
 
-    # Listen for incoming connections
-    server.listen(5)
-
-    # Accept the connection
-    client, address = server.accept()
-
-    mbot = MBot()
-
-    # Receive data from the client
     while True:
-        data = client.recv(1024)
-        json_data = json.loads(data.decode())
+
+        try:
+            in_msg = websocket.recv(timeout=0.1)
+            with in_msgs_lock:
+                in_msgs.append(in_msg)
+        except TimeoutError as e:
+            pass
+        except Exception as e:
+            break;
+        
+        with out_msgs_lock:
+            if (len(out_msgs) > 0):
+                websocket.send(out_msgs.pop(0))
+        
+def parse_msgs():
+    global mbot, msgs
+    while True:
+        with in_msgs_lock:
+            if (len(in_msgs) > 0):
+                message = in_msgs.pop(0)
+            else:
+                continue
+
+        json_data = json.loads(message)
         if (json_data.get('cmd') == "drive"):
             vx = json_data.get('args').get('vx')
             vy = json_data.get('args').get('vy')
@@ -32,10 +51,12 @@ def main():
         elif (json_data.get('cmd') == "read_lidar"):
             print("Reading Lidar")
             ranges, thetas = mbot.read_lidar()
-            client.send(json.dumps({
+            msg = json.dumps({
                                     'ranges': ranges,
                                     'thetas': thetas
-                                    }).encode())
+                                    }).encode()
+            with out_msgs_lock:
+                out_msgs.append(msg)
         elif (json_data.get('cmd') == "read_odometry"):
             x, y, theta = mbot.read_odometry()
             print("Reading Odometry")
@@ -54,19 +75,26 @@ def main():
         elif (json_data.get('cmd') == "read_slam_pose"):
             x, y, theta = mbot.read_slam_pose()
             print("Reading SLAM Pose")
-            client.send(json.dumps({
-                                    'x': x,
-                                    'y': y,
-                                    'theta': theta
-                                    }).encode())
+            msg = json.dumps({
+                                'x': x,
+                                'y': y,
+                                'theta': theta
+                                }).encode()
+            with out_msgs_lock:
+                out_msgs.append(msg)
         elif (json_data.get ('cmd') == "read_hostname"):
             hostname = mbot.read_hostname()
-            client.send(json.dumps({
-                                    'hostname': hostname
-                                    }).encode())
-            
-    # Close the connection
-    client.close()
+            msg = json.dumps({
+                                'hostname': hostname
+                                }).encode()
+            with out_msgs_lock:
+                out_msgs.append(msg)
+                    
+def main():
+    parse_thread = Thread(target=parse_msgs, name='parse_thread', daemon=True)
+    parse_thread.start()
+    with serve(bridge, "localhost", 8765) as server:
+        server.serve_forever()
 
 if __name__ == '__main__':
     main()
